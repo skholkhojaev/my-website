7173277bccc40b3485bafaf06df5405e14ff44d5
skholkhojaev <skholkhojaev@adobe.com>
2024-08-26 11:35:11 +0200

    Upadte2

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -37,7 +37,7 @@ function constructPayload(form) {
 async function submitForm(form) {
   const payload = constructPayload(form);
   payload.timestamp = new Date().toJSON();
-  const resp = await fetch(`https://admin.hlx.page/${form.dataset.action}/skholkhojaev/my-website/main/Form.json`, {
+  const resp = await fetch(`https://main--my-website--skholkhojaev.hlx.page/form.json`, {
     method: 'POST',
     cache: 'no-cache',
     headers: {
